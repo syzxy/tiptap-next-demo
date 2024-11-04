7b50b404b8820bee4612df980ef78c6c5c3bf54b
Yunze Shi <yunzeshi@outlook.com>
2024-11-04 22:05:05 +1100

    feat: ensure page title is the only h1

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -1,12 +1,42 @@
 "use client";
 
+import { Node } from "@tiptap/core";
 import Document from "@tiptap/extension-document";
+import Heading from "@tiptap/extension-heading";
 import Placeholder from "@tiptap/extension-placeholder";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
+const DocTitle = Node.create({
+  name: "title",
+  content: "text*",
+  group: "block",
+  defining: true,
+  parseHTML() {
+    return [{ tag: "h1" }];
+  },
+  renderHTML({ HTMLAttributes }) {
+    return ["h1", HTMLAttributes, 0];
+  },
+});
+
+/**
+ * Force a heading i.e the page heading as the firt node.
+ */
 const CustomDocument = Document.extend({
-  content: "heading block*",
+  content: "title block*",
+});
+
+/**
+ * Maps the heading level 1 to h2, and level 2 to h3 etc.
+ * To allow the page title as h1.
+ */
+const CustomHeading = Heading.extend({
+  renderHTML({ node, HTMLAttributes }) {
+    const level = node.attrs.level;
+    const htmlTag = `h${Math.min(level + 1, 6)}`;
+    return [htmlTag, HTMLAttributes, 0];
+  },
 });
 
 const Editor = () => {
@@ -18,14 +48,26 @@ const Editor = () => {
       },
     },
     extensions: [
+      StarterKit.configure({ document: false, heading: false }),
       CustomDocument,
-      StarterKit.configure({ document: false }),
+      DocTitle,
+      CustomHeading.configure({
+        levels: [1, 2, 3],
+      }),
       Placeholder.configure({
         emptyNodeClass:
           "before:content-[attr(data-placeholder)] before:float-left before:text-neutral-300 before:h-0 before:pointer-events-none",
         placeholder({ node }) {
-          if (node.type.name === "heading") return "New page";
-          return "Write something, or press '/' for commands...";
+          switch (node.type.name) {
+            case "title":
+              return "New document";
+            case "heading": {
+              return `Heading ${node.attrs.level}`;
+            }
+
+            default:
+              return "Write something, or press '/' for commands...";
+          }
         },
       }),
     ],
